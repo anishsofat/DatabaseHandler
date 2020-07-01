11156846202ae0ac1f61c3d6d0b26876e6bc0e41
anishsofat <sofatanish16@gmail.com>
2020-07-01 22:55:55 +0530

    included files in index document

---
+++ includes/signup.inc.php
@@ -0,0 +1,13 @@
+<?php
+	include_once 'dbconn.inc.php';
+
+	$first = $_POST['first'];
+	$last = $_POST['last'];
+	$email = $_POST['email'];
+	$uid = $_POST['uid'];
+	$pwd = $_POST['pwd'];
+
+	$sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$first', '$last', '$email', '$uid', '$pwd');";
+	mysqli_query($conn, $sql);
+
+	header("Location: ../index.php?signnup=success");
