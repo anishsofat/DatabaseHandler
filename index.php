61bfebfa517186b43f9b530b19f4f9d324f30537
anishsofat <sofatanish16@gmail.com>
2020-07-02 01:03:26 +0530

    homepage of the website

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Database Handler</title>
+</head>
+<body>
+	<h1>This is a database handler</h1>
+	<p>Want to signup or Login</p>
+	<form>
+		<button formaction="includes/login.inc.php"><p>Login</p></button>
+		<br>
+		<button formaction="includes/signup.inc.php"><p>Sign-Up</p></button>
+		<br>
+	</form>
+</body>
+</html>
