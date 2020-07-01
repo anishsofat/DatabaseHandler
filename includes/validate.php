fb09d4501ca13fbf02a7290dc8de937d9370e3af
anishsofat <sofatanish16@gmail.com>
2020-07-02 02:53:09 +0530

    debug code

--- includes/validate.php
+++ includes/validate.php
@@ -4,13 +4,17 @@
 	$password = $_POST['user_pwd'];
 	$sql = "SELECT * FROM users;";
 	$result = mysqli_query($conn, $sql);
+	$count = 1;
 	if(mysqli_num_rows($result)>0)
 	{
 		while($row = mysqli_fetch_assoc($result)) {
-			if($row['user_first'] == $username and $row['user_pwd'] == $password)
+			if($row['user_uid'] == $username and $row['user_pwd'] == $password)
 			{
 				header("Location: ../actions.php?login=success");
+				$count = 0;
 			}
 		}
 	}
-	header("Location: ../login.php?login=failed");
+	if($count){
+		header("Location: ../login.php?login=failed");
+	}
