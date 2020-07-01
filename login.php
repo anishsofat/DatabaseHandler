33add8dcfa982f225f24f3325e1177fc9fadbacd
anishsofat <sofatanish16@gmail.com>
2020-07-02 01:45:35 +0530

    login feature

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+<h1>Login</h1>
+<form action="includes/validate.php" method="POST">
+	<input type="text" name="user_name" placeholder="Username">
+	<br>
+	<input type="password" name="user_pwd" placeholder="Password">
+	<br>
+	<button type="submit" name="submit">Login</button>
+</form>
+</body>
+</html>
