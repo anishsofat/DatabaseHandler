c61af7a86809d9103431f17968243eae419a0fa3
anishsofat <sofatanish16@gmail.com>
2020-07-02 02:20:26 +0530

    Have features to insert contacts or allot contacts from the main databse

---
+++ actions.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Features</title>
+</head>
+<body>
+<h1>Features</h1>
+<p>Want to Insert contacts or Need contacts</p>
+	<form>
+		<button formaction="insert.inc.php"><p>Insert</p></button>
+		<br>
+		<button formaction="allot.php"><p>Need Contacts</p></button>
+		<br>
+	</form>
+</body>
+</html>
