e96e7cc287f8c4e179cabbda2e959f21cb6c5496
anishsofat <sofatanish16@gmail.com>
2020-07-02 01:24:09 +0530

    Made all field madatory

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,13 +1,17 @@
 <?php
-	include_once 'dbconn.inc.php';
+	include 'dbconn.inc.php';
 
 	$first = $_POST['first'];
 	$last = $_POST['last'];
 	$email = $_POST['email'];
 	$uid = $_POST['uid'];
 	$pwd = $_POST['pwd'];
-
-	$sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$first', '$last', '$email', '$uid', '$pwd');";
-	mysqli_query($conn, $sql);
-
-	header("Location: ../index.php?signnup=success");
+	if($first != "" and $last != "" and $email != "" and $uid != "" and $pwd != "" ){
+		$sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) 
+		VALUES ('$first', '$last', '$email', '$uid', '$pwd');";
+		mysqli_query($conn, $sql);
+		header("Location: ../index.php?signnup=success");
+	}
+	
+	header("Location: ../signup.php?signnup=try+again");
+	
